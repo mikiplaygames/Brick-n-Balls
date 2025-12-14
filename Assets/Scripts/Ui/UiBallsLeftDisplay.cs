a2a2ced6e7325b3c14a64377a708a980a27be2ce
mikiplaygames <95536396+mikiplaygames@users.noreply.github.com>
2025-12-14 20:52:38 +0100

    Fixed ui's

--- Assets/Scripts/Ui/UiBallsLeftDisplay.cs
+++ Assets/Scripts/Ui/UiBallsLeftDisplay.cs
@@ -2,6 +2,7 @@ public class UiBallsLeftDisplay : NumberDisplay {
     protected override string BaseText => "Balls left";
     protected override int GetNumber() => GameManager.BallsLeft;
     protected override void Awake(){
+        base.Awake();
         PlayerShootingSystem.OnShoot.AddListener(UpdateDisplay);
         GameManager.OnGameStart.AddListener(UpdateDisplay);
     }
--- Assets/Scripts/Ui/UiPointsDisplay.cs
+++ Assets/Scripts/Ui/UiPointsDisplay.cs
@@ -3,6 +3,7 @@ public class UiPointsDisplay : NumberDisplay
     protected override string BaseText => "Points";
     protected override int GetNumber() => GameManager.Score;
     protected override void Awake(){
+        base.Awake();
         BrickCollisionSystem.OnBrickHit.AddListener(UpdateDisplay);
     }
 }
